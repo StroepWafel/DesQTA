e809eb8bea10f115fafedd780a1a528a47c36775
Aden Linday <lindsaya542@gmail.com>
2025-11-25 19:46:23 +1030

    fix: restore getWithIdbFallback import for other functions

--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -3,7 +3,7 @@
   import { onMount } from 'svelte';
 
   // $lib/ imports
-  import { setIdb } from '$lib/services/idbCache';
+  import { getWithIdbFallback, setIdb } from '$lib/services/idbCache';
   import T from '$lib/components/T.svelte';
   import { _ } from '$lib/i18n';
 
