cadaf2c710187adb19e6d8d0adddfa8f2f706a50
Aden Linday <lindsaya542@gmail.com>
2025-12-18 15:32:24 +1030

    fix: position toast notifications below app bar

--- src/lib/components/ToastContainer.svelte
+++ src/lib/components/ToastContainer.svelte
@@ -18,7 +18,7 @@
 
 {#if toasts.length > 0}
   <div
-    class="fixed top-4 right-4 z-50 flex flex-col gap-2 pointer-events-none"
+    class="fixed top-20 right-4 z-50 flex flex-col gap-2 pointer-events-none"
     role="region"
     aria-label="Notifications"
   >
