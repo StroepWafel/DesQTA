379cc3ef5ed2c3473db82f674e6f548f3d8b534e
StroepWafel <109832156+StroepWafel@users.noreply.github.com>
2026-04-01 11:02:23 +1030

    fix: Decode HTML Entities for Feedback

--- src/lib/components/AssessmentDetails.svelte
+++ src/lib/components/AssessmentDetails.svelte
@@ -73,7 +73,7 @@
         class="p-4 mb-4 rounded-xl transition-all duration-300 dark:bg-zinc-800 bg-zinc-200 hover:shadow-lg hover:shadow-accent-500/5">
         <div class="mb-1 font-semibold">Teacher feedback</div>
         <div class="dark:text-zinc-300 text-zinc-700">
-          {assessmentData.engagement.feedbackComment}
+          {decodeHtmlEntities(assessmentData.engagement.feedbackComment)}
         </div>
       </div>
     {:else if assessmentData.marked}
